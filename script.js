483752e23c434e604ecd042b52cb3788f6208f5d
Serhii <s.krokhmalnyi@gmail.com>
2016-05-30 13:28:11 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var a, b, c, d, D, x1, x2;
-
 a = prompt('type a' ,a);
 b = prompt('type b' ,b);
 c = prompt('type c' ,c);
@@ -7,8 +6,8 @@ D = b * b - 4 * a * c;
 d= Math.sqrt(D);
 if (D >=0) 
 {
-x1= (-b + d) / 2 * a;
-x2= (-b - d) / 2 * a;
+x1= (-b + d) / (2 * a);
+x2= (-b - d) / (2 * a);
 alert(x1+" "+x2);
 }
 else
